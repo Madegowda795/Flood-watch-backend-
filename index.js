cdcbbc92ed19b1d90fefe994959537cf0ce6093a
Madegowda795 <madegowda9441@gmail.com>
2026-05-26 07:12:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const API_KEY = 'dEasGMt9cFUqj7rz6pTgZB5u38fnHkxleb0JhCwDyKiNQ2VSOLMbpPfIvXBh79JH3qjYlSWgZuQkyAL6';
+
+app.get('/send-otp', async (req, res) => {
+  const { phone, otp } = req.query;
+  try {
+    const response = await fetch(
+      `https://www.fast2sms.com/dev/bulkV2?authorization=${API_KEY}&route=otp&variables_values=${otp}&flash=0&numbers=${phone}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    res.json({ return: false, message: e.message });
+  }
+});
+
+app.listen(3000, () => console.log('Server running'));
